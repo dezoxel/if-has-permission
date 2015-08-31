3c26788ba774bc838ca4fe47b9f6c49571e045f0
dezoxel <dezoxel@gmail.com>
2015-08-31 19:13:10 +0300

    [ifHasPermission] Single string and array of perms implemented

--- src/app/if_has_permission.directive.js
+++ src/app/if_has_permission.directive.js
@@ -3,8 +3,28 @@
 
   angular
     .module('ifHasPermission')
-    .directive('ifHasPermission', function(ngIfDirective) {
+    .directive('ifHasPermission', function(ngIfDirective, userPermissions) {
       var ngIfDirective = ngIfDirective[0];
+
+      function has(userPerms, requiredPerms) {
+        if (typeof requiredPerms === 'string') {
+          return hasOne(userPerms, requiredPerms);
+
+        } else if (requiredPerms instanceof Array) {
+
+          return requiredPerms.some(function(perm) {
+            return hasOne(userPerms, perm);
+          });
+        }
+      }
+
+      function hasOne(userPerms, perm) {
+        return userPerms.indexOf(perm) !== -1;
+      }
+
+      function evalRequiredPermissions(scope, attr) {
+        return scope.$eval(attr.ifHasPermission);
+      }
 
       return {
         transclude: ngIfDirective.transclude,
@@ -13,22 +33,14 @@
         restrict: ngIfDirective.restrict,
         link: function($scope, $element, $attr) {
 
-          var ngIf = evalNgIf();
+          var ngIf = $attr.ngIf ? $scope.$eval($attr.ngIf) : true;
 
-          // TODO: Implement main logic of directive
-          function hasPermission() {
-            return $scope.$eval($attr.ifHasPermission);
+          $attr.ngIf = function() {
+            var userPerms = userPermissions.get();
+            var requiredPerms = evalRequiredPermissions($scope, $attr);
+
+            return ngIf && has(userPerms, requiredPerms);
           }
-
-          function evalNgIf() {
-            return $attr.ngIf ? $scope.$eval($attr.ngIf) : true;
-          }
-
-          function evalFunction() {
-            return ngIf && hasPermission();
-          }
-
-          $attr.ngIf = evalFunction;
 
           ngIfDirective.link.apply(null, arguments);
         }
--- src/app/main.controller.js
+++ src/app/main.controller.js
@@ -4,19 +4,7 @@
   angular
     .module('ifHasPermission')
     .controller('MainController', function(userPermissions) {
-      console.log('Initial perms:', userPermissions.get());
-
-      userPermissions.set(['A', 'B', 'C']);
-      console.log('Fully updated perms:', userPermissions.get());
-
-      userPermissions.addOne('D');
-      console.log('Added one perm:', userPermissions.get());
-
-      userPermissions.removeByName('B');
-      console.log('Removed one perm:', userPermissions.get());
-
-      this.visible = true;
-      this.allowEdit = true;
+      console.log('User perms:', userPermissions.get());
     });
 
 })();
